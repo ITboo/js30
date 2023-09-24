b6cb0b534325279aeb15c04839bfca9b2bdf2e9e
ITboo <annaacdc@tut.by>
2023-09-24 16:02:28 +0300

    feat: add covers (Sun, Sep 24, 2023 04:02:28 PM)

--- audio-player/app/main.js
+++ audio-player/app/main.js
@@ -5,26 +5,45 @@ const playBtn = document.querySelector('.play')
 const prevBtn = document.querySelector('.prev')
 const nextBtn = document.querySelector('.next')
 
+const randomBtn = document.querySelector('.random')
+
 const title = document.querySelector('.player__title')
-const cover = document.querySelector('.player__cover')
+const cover = document.querySelector('.cover__img')
 
 const progressContainer = document.querySelector('.player__progress-container')
 const progress = document.querySelector('.progress-bar')
 
-const songs = ['lesson-1', 'lesson-2', 'lesson-3']
+const start = document.querySelector('.start-value')
+const end = document.querySelector('.end-value')
+
+const songs = [
+    {
+        title: 'lesson-1',
+        cover: '1'
+    },
+    {
+        title: 'lesson-2',
+        cover: '2'
+    },
+    {
+        title: 'lesson-3',
+        cover: '3'
+    },]
 let songIndex = 0
 let isPlaying = false;
 
-function loadSong(song) {
-    title.innerHTML = song
-    audio.src = `./music/${song}.mp3`
-}
 loadSong(songs[songIndex])
+
+function loadSong(song) {
+    title.innerHTML = `세종한국어: ${song.title}`
+    audio.src = `./music/${song.title}.mp3`
+    cover.src=`./assets/img/${song.cover}.png`
+}
+
 
 //play
 function playSong() {
     isPlaying = true
-    audio.currentTime = 0
     audio.play()
 }
 
@@ -34,14 +53,11 @@ function pauseSong() {
     audio.pause()
 }
 playBtn.addEventListener('click', () => {
-    if (!isPlaying) {
-        playSong()
-    } else {
-        pauseSong()
-    }
+    isPlaying ? pauseSong() : playSong()
 })
 nextBtn.addEventListener('click', playNext)
 
+//next-prev
 function playNext() {
     songIndex++
     if (songIndex > songs.length - 1) {
@@ -78,6 +94,30 @@ function setProgress(e) {
     audio.currentTime = (clickX / width) * duration
 }
 progressContainer.addEventListener('click', setProgress)
+
+//timer
+function reset() {
+    start.textContent = "00:00";
+    end.textContent = "00:00";
+}
+function setUpdate(e) {
+    const { currentTime, duration } = e.srcElement
+
+    let currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+    start.textContent = currentMinutes + ":" + currentSeconds;
+    end.textContent = durationMinutes + ":" + durationSeconds;
+}
+
+
 
 //autoplay
 audio.addEventListener('ended', playNext)
