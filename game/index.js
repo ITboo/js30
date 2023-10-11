5a2ee61e20a19976a29cbb2ff6a2638341a259da
ITboo <annaacdc@tut.by>
2023-10-11 16:42:41 +0300

    feat: add random and flipping cards  (Wed, Oct 11, 2023 04:42:41 PM)

--- game/index.js
+++ game/index.js
@@ -1,21 +1,28 @@
 import { cardArray } from "./script/cards.js";
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
+  // TODO add random
+  cardArray.sort(() => 0.5 - Math.random())
   // TODO create board
   console.log('Game has been started')
+  console.log('Cards has been randomized')
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       const card = document.createElement('img')
       card.setAttribute('src', 'assets/img/1.jpg')
       card.setAttribute('data-id', i)
       card.classList.add('card__img')
+      card.addEventListener('click', flipCard)
       grid.appendChild(card)
     }
     console.log('Grid has been created')
   }
 
   // TODO flip cards
-
+  function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    this.setAttribute('src', cardArray[cardId].img)
+  }
   // TODO local storage
 
   // TODO score update
