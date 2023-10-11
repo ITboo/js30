c2d56dee81e28058617d6950faa2090dd2f45b1a
ITboo <annaacdc@tut.by>
2023-10-11 19:53:47 +0300

    feat: add random facts and reload btn  (Wed, Oct 11, 2023 07:53:47 PM)

--- game/index.js
+++ game/index.js
@@ -1,28 +1,43 @@
 import { cardArray } from "./script/cards.js";
+import { factsArray } from "./script/facts.js";
+
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
-  // TODO add random
+  const restart = document.querySelector('.board__btn')
+  const fact = document.querySelector('.fact')
+
+
+  // add random
   cardArray.sort(() => 0.5 - Math.random())
-  // TODO create board
+  // create board
   console.log('Game has been started')
   console.log('Cards has been randomized')
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       const card = document.createElement('img')
-      card.setAttribute('src', 'assets/img/1.jpg')
+      card.setAttribute('src', 'assets/img/blank.jpg')
       card.setAttribute('data-id', i)
       card.classList.add('card__img')
       card.addEventListener('click', flipCard)
       grid.appendChild(card)
     }
     console.log('Grid has been created')
+    randomFact()
   }
 
-  // TODO flip cards
+  // flip cards
   function flipCard() {
     let cardId = this.getAttribute('data-id')
     this.setAttribute('src', cardArray[cardId].img)
   }
+
+  function randomFact() {
+    const random = Math.floor(Math.random() * (factsArray.length - 1));
+    const randomFact = factsArray[random];
+    fact.textContent = randomFact.eng;
+  }
+
+
   // TODO local storage
 
   // TODO score update
@@ -30,7 +45,11 @@ document.addEventListener('DOMContentLoaded', () => {
   // TODO random fact
 
   // TODO buttons
-
+  restart.addEventListener('click', reload);
+  function reload() {
+    document.location.reload();
+    console.log('Start new game')
+  }
   // TODO settings
 
   createBoard();
---
+++ game/script/facts.js
@@ -0,0 +1,23 @@
+export const factsArray = [
+    {
+        id: '1',
+        eng: 'assets/img/1.jpg'
+    },
+    {
+        id: '2',
+        eng: 'assets/img/2.jpg'
+    },
+    {
+        id: '3',
+        eng: 'assets/img/3.jpg'
+    },
+    {
+        id: '4',
+        eng: 'assets/img/4.jpg'
+    },
+    {
+        id: '5',
+        eng: 'assets/img/5.jpg'
+    },
+
+]
