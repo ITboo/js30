80cee14a2eab51d5890d3ba4e42251770a530032
ITboo <annaacdc@tut.by>
2023-10-15 19:25:15 +0300

    feat: add localStorage (Sun, Oct 15, 2023 07:25:15 PM)

--- game/main.js
+++ game/main.js
@@ -3,11 +3,15 @@ import { factsArray } from "./script/facts.js";
 const gridContainer = document.querySelector(".grid");
 const restartBtn = document.querySelector('.board__btn')
 const fact = document.querySelector('.fact')
+const data_place = document.querySelectorAll('.place-count');
 
 let cards = [];
+let matches = 0;
 let firstCard, secondCard;
 let lockBoard = false;
 let score = 0;
+let records = localStorage.getItem('records');
+
 
 document.querySelector(".steps__count").textContent = score;
 
@@ -47,31 +51,37 @@ function generateCards() {
     cardElement.addEventListener("click", flipCard);
   }
   randomFact()
+  console.log('random fact generated')
+  console.log('get local storage records', records);
 }
 
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
-
   this.classList.add("flipped");
-
   if (!firstCard) {
     firstCard = this;
     return;
   }
-
   secondCard = this;
   score++;
   document.querySelector(".steps__count").textContent = score;
   lockBoard = true;
-
+  console.log(matches)
   checkForMatch();
 }
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.name === secondCard.dataset.name;
 
-  isMatch ? disableCards() : unflipCards();
+  if (isMatch) {
+
+    disableCards()
+    ++matches
+  } else { unflipCards(); }
+  if (matches === 8) {
+    finishGame()
+  }
 }
 
 function disableCards() {
@@ -99,17 +109,61 @@ function restart() {
   resetBoard();
   shuffleCards();
   score = 0;
-  document.querySelector(".steps__count").textContent = score;
+  document.querySelector(".steps__count").textContent = '0';
   gridContainer.innerHTML = "";
   generateCards();
 }
 
-  // buttons
-  restartBtn.addEventListener('click', restart);
+// buttons
+restartBtn.addEventListener('click', restart);
 
-  // random fact
-  function randomFact() {
-    const random = Math.floor(Math.random() * (factsArray.length - 1));
-    const randomFact = factsArray[random];
-    fact.textContent = randomFact.eng;
-  }  
+// random fact
+function randomFact() {
+  const random = Math.floor(Math.random() * (factsArray.length - 1));
+  const randomFact = factsArray[random];
+  fact.textContent = randomFact.eng;
+}
+
+// local storage
+
+
+if (!records) {
+  console.log('local storage empty');
+  records = [];
+} else {
+  records = records.split(',');
+}
+
+if (records.length > 0) {
+  for (let i = 0; i < records.length; i++) {
+    data_place[i].textContent = records[i];
+  }
+}
+
+const newScore = () => {
+  console.log('newScore', score);
+  if (records.length >= 10) {
+    records.shift();
+    records.push(score);
+    console.log('finish records', records);
+    for (let i = 0; i < records.length - 1; i++) {
+      data_place[i].textContent = data_place[i + 1].textContent;
+      console.log(i);
+    }
+  } else {
+    records.push(score);
+    console.log('finish records', records);
+  }
+  data_place[records.length - 1].textContent = score;
+}
+
+const newLocalStorage = () => {
+  localStorage.setItem('records', records);
+  console.log('set local storage records', records);
+}
+
+const finishGame = () => {
+  console.log('finish game');
+  newScore();
+  newLocalStorage();
+}
--- game/script/score.js
+++ game/script/score.js
@@ -1,13 +1,25 @@
 const scoreBtn = document.querySelector('#score')
 const scoreModal = document.getElementById('scoreModal');
 const closeBtn = document.querySelector('.close')
+const clear = document.querySelector('.score__btn')
+const zero = document.querySelectorAll('.place-count')
 
-scoreBtn.onclick = function() {
+scoreBtn.onclick = function () {
     scoreModal.style.display = "block";
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == scoreModal) {
         scoreModal.style.display = "none";
     }
 }
+
+clear.addEventListener('click', () => {
+    for (let i = 0; i < 10; i++) {
+        zero[i].textContent = '0';
+    }
+    localStorage.clear();
+    console.log('Clear localStorage')
+    console.log(localStorage)
+})
+
